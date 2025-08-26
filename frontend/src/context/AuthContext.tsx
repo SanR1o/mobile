81299b4a18db8e75728e7f6a4d4fa0771836cc5a
SanR1o <santiagojriola@gmail.com>
2025-08-26 16:29:00 -0500

    contexto

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -32,4 +32,46 @@ export const AuthProvider: React.FC<AuthProviderProps> = {{ children }} => {
     useEffect(() => {
         checkAuthStatus();
         }, []);
-}
+
+    const checkAuthStatus = async () => {
+        try {
+            setIsLoading(true);
+
+            const isAuth = await authService.isAuthenticated();
+
+            if (isAuth) {
+                const isValidToken = await authService.verifyToken();
+
+                if (isValidToken) {
+                    const userData = await authService.getCurrentUser();
+                    setUser(userData);
+                } else {
+                    await authService.clearAuthData();
+                    setUser(null);
+                }
+
+            } else {
+                setUser(null)
+            }
+        } catch (error) {
+            console.error('Error verificando autenticacion: ', error);
+            await authService.clearAuthData();
+            setUser(null);
+        } finally {
+            setIsLoading(false)
+        }
+    };
+
+    //proceso de inicio de sesion
+    const login = async (credentials: LoginCredentials):
+    Promise<LoginResponse> => {
+        try{
+            setIsLoading(true);
+
+            const response = await authService.login(credentials);
+            if (response.success && response.data) {
+                setUser(response.data.user);
+            }
+        } catch (error) {}
+    }
+};
